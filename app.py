0b3662e30c23cce8b8d549a96b621093915abf64
Xiomara <xiomaracortes0@gmail.com>
2021-09-06 12:52:35 -0500

    Update the code

--- app.py
+++ app.py
@@ -1,39 +1,38 @@
-import os
-
 from flask import Flask
 from flask_restful import Api
-from flask_jwt import JWT
+from .modelos import db
+from .vistas import VistaCanciones, VistaCancionesCompartidasUsuario, VistaCancion, VistaSignIn, VistaAlbum, VistaAlbumsUsuario, VistaCancionesAlbum, VistaLogIn, VistaAlbumesCanciones, VistaCancionesUsuario, VistaUsuariosCancionCompartida
+from flask_jwt_extended import JWTManager
+from flask_cors import CORS, cross_origin
 
-from security import authenticate, identity
-from resources.user import UserRegister
-from resources.item import Item, ItemList
-from resources.store import Store, StoreList
 
-app = Flask(__name__)
+def create_app(config_name):
+    app = Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tutorial_canciones.db'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['JWT_SECRET_KEY'] = 'frase-secreta'
+    app.config['PROPAGATE_EXCEPTIONS'] = True
+    return app
 
-app.config['DEBUG'] = True
+app = create_app('default')
+app_context = app.app_context()
+app_context.push()
 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.secret_key = 'jose'
+db.init_app(app)
+db.create_all()
+cors = CORS(app)
+
 api = Api(app)
+api.add_resource(VistaCanciones, '/canciones')
+api.add_resource(VistaCancion, '/cancion/<int:id_cancion>')
+api.add_resource(VistaAlbumesCanciones, '/cancion/<int:id_cancion>/albumes')
+api.add_resource(VistaSignIn, '/signin')
+api.add_resource(VistaLogIn, '/logIn')
+api.add_resource(VistaAlbumsUsuario, '/usuario/<int:id_usuario>/albumes')
+api.add_resource(VistaCancionesUsuario, '/usuario/<int:id_usuario>/canciones')
+api.add_resource(VistaAlbum, '/album/<int:id_album>')
+api.add_resource(VistaCancionesAlbum, '/album/<int:id_album>/canciones')
+api.add_resource(VistaUsuariosCancionCompartida, '/cancion/<int:id_cancion>/usuarios')
+api.add_resource(VistaCancionesCompartidasUsuario, '/usuario/<int:id_usuario>/cancionescompartidas')
 
-jwt = JWT(app, authenticate, identity)  # /auth
-
-api.add_resource(Store, '/store/<string:name>')
-api.add_resource(Item, '/item/<string:name>')
-api.add_resource(ItemList, '/items')
-api.add_resource(StoreList, '/stores')
-
-api.add_resource(UserRegister, '/register')
-
-if __name__ == '__main__':
-    from db import db
-    db.init_app(app)
-
-    if app.config['DEBUG']:
-        @app.before_first_request
-        def create_tables():
-            db.create_all()
-
-    app.run(port=5000)
+jwt = JWTManager(app)
