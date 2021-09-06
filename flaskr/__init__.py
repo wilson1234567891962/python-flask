5742efd58a8012b62c02b409c0d08a83a6f5295f
Xiomara <xiomaracortes0@gmail.com>
2021-09-06 17:00:51 -0500

    Update the code

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,6 +1,5 @@
 from flask import Flask
 
-
 def create_app(config_name):
     app = Flask(__name__)
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tutorial_canciones.db'
--- flaskr/app.py
+++ flaskr/app.py
@@ -1,18 +1,13 @@
-from flask import Flask
+from flaskr import create_app
 from flask_restful import Api
 from .modelos import db
-from .vistas import VistaCanciones, VistaCancionesCompartidasUsuario, VistaCancion, VistaSignIn, VistaAlbum, VistaAlbumsUsuario, VistaCancionesAlbum, VistaLogIn, VistaAlbumesCanciones, VistaCancionesUsuario, VistaUsuariosCancionCompartida
 from flask_jwt_extended import JWTManager
 from flask_cors import CORS, cross_origin
-
-
-def create_app(config_name):
-    app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tutorial_canciones.db'
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    app.config['JWT_SECRET_KEY'] = 'frase-secreta'
-    app.config['PROPAGATE_EXCEPTIONS'] = True
-    return app
+from .vistas import VistaCanciones, VistaCancionesCompartidasUsuario, VistaCancion, VistaSignIn, VistaAlbum, \
+    VistaAlbumsUsuario, VistaCancionesAlbum, VistaLogIn, VistaAlbumesCanciones, VistaCancionesUsuario, \
+    VistaUsuariosCancionCompartida
+from flask_jwt_extended import JWTManager
+from flask_cors import CORS, cross_origin
 
 app = create_app('default')
 app_context = app.app_context()
